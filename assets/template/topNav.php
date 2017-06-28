274fc55fa9c6356100c351c1e9a511f43b5995ed
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-28 16:17:28 -0700

    Fixed left aligned link spacing

--- assets/template/topNav.php
+++ assets/template/topNav.php
@@ -6,55 +6,58 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
 		<link rel="stylesheet" type="text/css"  href="./assets/css/poverty.css">
 		<meta name="description" content="UMS / CRM software geared towards giving you the ability to efficiently manage your proporties.">
-        <meta name="author" content="Michael Brooks, Esteban Perez">
+		<meta name="author" content="Michael Brooks, Esteban Perez">
 	</head>
 	<body>
 
 
-<!--Side Navigation Bar-->
-        <div class="sidebar " id="side">
-            <div class="sideHeader" style="text-align:center;">
-                <div class="sideHeader" style="text-align:center;">
-                    <h1 class="white-text"><a href="./index" class="logo white-text">Patel Tennis</a></h1>
-                </div>
-            </div>
-            <div class="sideBody">
-                <ul >
-                    <li><a href="./index">Home</a></li>
-                    <li><a href="./profile">My Profile</a></li>
-                    <li><a href="./students">Students</a></li>
-                    <li><a href="./attendance">Attendance</a></li>
-                    <li><a href="./coaches">Coaches</a></li>
-                    <li><a href="../db/logout">Logout</a></li>
-                </ul>
-            </div>
-        </div>
+		<!--Side Navigation Bar-->
+		<div class="sidebar " id="side">
+			<div class="sideHeader" style="text-align:center;">
+				<div class="sideHeader" style="text-align:center;">
+					<h1 class="white-text"><a href="./index" class="logo white-text">Patel Tennis</a></h1>
+				</div>
+			</div>
+			<div class="sideBody">
+				<ul >
+					<li><a href="./products">Products</a></li>
+					<li><a href="./solutions">Solutions</a></li>
+					<li><a href="./customers">Customers</a></li>
+					<li><a href="./pricing">Pricing</a></li>
+					<li><a href="./resources">Resources</a></li>
+					<li><a href="./about">About</a></li>
+					<li><a href="./database/logout">Logout</a></li>
+				</ul>
+			</div>
+		</div>
 
-        <div class="site-content" style="" >
-            <!--Top Navigation Bar-->
-            <nav class="">
-                <div class="nav-wrapper">
-                    <a href="./index" class="logo"><img src="./assets/img/logo.png" style="height:100%;"></a>
-<!--                    <ul class="left hide-on-med-and-down">
-                        <li><a href="./index">Home</a></li>
-                        <li><a href="./profile">My Profile</a></li>
-                        <li><a href="./students">Students</a></li>
-                        <li><a href="./attendance">Attendance</a></li>
-                        <li><a href="./coaches">Coaches</a></li>
-                        <li><a href="../db/logout">Logout</a></li>
-                    </ul>-->
-                     <ul class="right hide-on-med-and-down">
-                        <li><a href="./index">Home</a></li>
-                        <li><a href="./profile">My Profile</a></li>
-                        <li><a href="./students">Students</a></li>
-                        <li><a href="./attendance">Attendance</a></li>
-                        <li><a href="./coaches">Coaches</a></li>
-                        <li><a href="../db/logout">Logout</a></li>
-                    </ul>
-                    <ul class="right hide-on-large-only">
-                        <li><a class="sideToggle"><i class="fa fa-bars blue-text" aria-hidden="true"></i></a></li>
+		<div class="site-content" style="" >
+			<!--Top Navigation Bar-->
+			<nav class="">
+				<div class="nav-wrapper">
+					<a href="./index" class="logo"><img src="./assets/img/logo.png" style="height:100%;"></a>
+					<ul class="left hide-on-med-and-down">
+						<li><a href="./products">Products</a></li>
+						<li><a href="./solutions">Solutions</a></li>
+						<li><a href="./customers">Customers</a></li>
+						<li><a href="./pricing">Pricing</a></li>
+						<li><a href="./resources">Resources</a></li>
+						<li><a href="./about">About</a></li>
+						<li><a href="./database/logout">Logout</a></li>
+					</ul>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="./products">Products</a></li>
+						<li><a href="./solutions">Solutions</a></li>
+						<li><a href="./customers">Customers</a></li>
+						<li><a href="./pricing">Pricing</a></li>
+						<li><a href="./resources">Resources</a></li>
+						<li><a href="./about">About</a></li>
+						<li><a href="./database/logout">Logout</a></li>
+					</ul>
+					<ul class="right hide-on-large-only">
+						<li><a class="sideToggle"><i class="fa fa-bars blue-text" aria-hidden="true"></i></a></li>
 
-                    </ul>
-                </div>
-            </nav>
-        </div>
+					</ul>
+				</div>
+			</nav>
+		</div>
