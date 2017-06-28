2141d8926e070e9b5988144fcbb8a99805178c6e
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-28 15:30:24 -0700

    Added top nav

--- assets/template/topNav.php
+++ assets/template/topNav.php
@@ -1,9 +1,52 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Page Title</title>
+		<title>UMS | Use My Software</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
 		<link rel="stylesheet" type="text/css"  href="./assets/css/poverty.css">
+		<meta name="description" content="UMS / CRM software geared towards giving you the ability to efficiently manage your proporties.">
+        <meta name="author" content="Michael Brooks, Esteban Perez">
 	</head>
 	<body>
+
+
+<!--Side Navigation Bar-->
+        <div class="sidebar " id="side">
+            <div class="sideHeader" style="text-align:center;">
+                <div class="sideHeader" style="text-align:center;">
+                    <h1 class="white-text"><a href="./index" class="logo white-text">Patel Tennis</a></h1>
+                </div>
+            </div>
+            <div class="sideBody">
+                <ul >
+                    <li><a href="./index">Home</a></li>
+                    <li><a href="./profile">My Profile</a></li>
+                    <li><a href="./students">Students</a></li>
+                    <li><a href="./attendance">Attendance</a></li>
+                    <li><a href="./coaches">Coaches</a></li>
+                    <li><a href="../db/logout">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="site-content" style="" >
+            <!--Top Navigation Bar-->
+            <nav class="">
+                <div class="nav-wrapper">
+                    <a href="./index" class="logo"><img src="../assets/img/logo.png" style="padding-right:10px;">LOGO</a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="./index">Home</a></li>
+                        <li><a href="./profile">My Profile</a></li>
+                        <li><a href="./students">Students</a></li>
+                        <li><a href="./attendance">Attendance</a></li>
+                        <li><a href="./coaches">Coaches</a></li>
+                        <li><a href="../db/logout">Logout</a></li>
+                    </ul>
+                    <ul class="right hide-on-large-only">
+                        <li><a class="sideToggle"><i class="fa fa-bars blue-text" aria-hidden="true"></i></a></li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
--- index.php
+++ index.php
@@ -5,6 +5,10 @@ include("./assets/template/topNav.php");
 	Hello This should be a card if it worked
 </div>
 
+<div class="container">
+	test
+</div>
+<p class="primary">test</p>
 <?php
 include("./assets/template/botNav.php");
 ?>
