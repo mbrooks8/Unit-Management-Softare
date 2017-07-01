8c48df4f8c6a05644d170189ea67df67eb73dc9e
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-30 20:13:04 -0700

    added font awesome cdn

--- assets/template/owner/topNav.php
+++ assets/template/owner/topNav.php
@@ -4,7 +4,7 @@
 		<title>UMS | Use My Software</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-		<link rel="stylesheet" type="text/css"  href="./assets/css/poverty.css">
+		<link rel="stylesheet" type="text/css"  href="../assets/css/poverty.css">
 		<meta name="description" content="UMS / CRM software geared towards giving you the ability to efficiently manage your proporties.">
 		<meta name="author" content="Michael Brooks, Esteban Perez">
 	</head>
@@ -15,18 +15,18 @@
 		<div class="sidebar " id="side">
 			<div class="sideHeader" style="text-align:center;">
 				<div class="sideHeader" style="text-align:center;">
-					<h1 class="white-text"><a href="./index" class="logo white-text">Patel Tennis</a></h1>
+					<h1 class="white-text"><a href="../index" class="logo white-text">Patel Tennis</a></h1>
 				</div>
 			</div>
 			<div class="sideBody">
 				<ul >
-					<li><a href="./products">Products</a></li>
-					<li><a href="./solutions">Solutions</a></li>
-					<li><a href="./customers">Customers</a></li>
-					<li><a href="./pricing">Pricing</a></li>
-					<li><a href="./resources">Resources</a></li>
-					<li><a href="./about">About</a></li>
-					<li><a href="./database/login">Log In</a></li>
+					<li><a href="../products">Products</a></li>
+					<li><a href="../solutions">Solutions</a></li>
+					<li><a href="../customers">Customers</a></li>
+					<li><a href="../pricing">Pricing</a></li>
+					<li><a href="../resources">Resources</a></li>
+					<li><a href="../about">About</a></li>
+					<li><a href="../database/login">Log In</a></li>
 				</ul>
 			</div>
 		</div>
@@ -35,19 +35,19 @@
 			<!--Top Navigation Bar-->
 			<nav class="">
 				<div class="nav-wrapper">
-					<a href="./index" class="logo"><img src="./assets/img/logo.png" style="height:100%;"></a>
+					<a href="./index" class="logo"><img src="../assets/img/logo.png" style="height:100%;"></a>
 					<ul class="left hide-on-med-and-down">
-						<li><a href="./products">Products</a></li>
-						<li><a href="./solutions">Solutions</a></li>
-						<li><a href="./customers">Customers</a></li>
-						<li><a href="./pricing">Pricing</a></li>
-						<li><a href="./resources">Resources</a></li>
-						<li><a href="./about">About</a></li>
+						<li><a href="../products">Products</a></li>
+						<li><a href="../solutions">Solutions</a></li>
+						<li><a href="../customers">Customers</a></li>
+						<li><a href="../pricing">Pricing</a></li>
+						<li><a href="../resources">Resources</a></li>
+						<li><a href="../about">About</a></li>
 					</ul>
 					<ul class="right hide-on-med-and-down">
-						<li><a href="./database/login">Log In</a></li>
+						<li><a href="../database/login">Log In</a></li>
 						<!--this should be a modal or it could be a page idk-->
-						<li><a href="./demo" class="button raised border">Request a Demo</a></li>
+						<li><a href="../demo" class="button raised border">Request a Demo</a></li>
 					</ul>
 					<ul class="right hide-on-large-only">
 						<li><a class="sideToggle"><i class="fa fa-bars blue-text" aria-hidden="true"></i></a></li>
