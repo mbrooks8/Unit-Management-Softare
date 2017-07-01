0fc08ce9e6f032f4a589cc23803d5c57f6d646b4
Afropenguin <mbrooks8@ucmerced.edu>
2017-07-01 13:18:31 -0700

    Added footer and accessibility features

--- assets/template/owner/botNav.php
+++ assets/template/owner/botNav.php
@@ -1,11 +1,29 @@
-</body>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<footer class="page-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col l8 s12">
+				<h5 class="white-text">4Dev</h5>
+				<p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+			</div>
+		</div>
+		<!--Google translate Pages-->
+		<div id="google_translate_element"></div><script type="text/javascript">
+		function googleTranslateElementInit() {
+			new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+		}
+		</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+	</div>
+	<div class="footer-copyright">
+		<div class="container">
+			© 2017 Copyright 4Dev All Rights Reserved
+			<a class="grey-text text-lighten-4 right" href="https://www.linkedin.com/in/estebanmagallonperez/">Please Hire Us</a>
+		</div>
+	</div>
+</footer>
 
-<!--Google translate Pages-->
-<!--<div id="google_translate_element"></div><script type="text/javascript">
-	function googleTranslateElementInit() {
-		new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
-	}
-</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>-->
+</body>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="../assets/js/fontChange.js" crossorigin="anonymous"></script>
 
 </html>
