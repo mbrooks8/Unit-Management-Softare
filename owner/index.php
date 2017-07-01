a7581406289fe0e2c3517f3a24a3918b550ea13b
Afropenguin <mbrooks8@ucmerced.edu>
2017-07-01 00:43:20 -0700

    Task Styling

--- owner/index.php
+++ owner/index.php
@@ -7,25 +7,23 @@ include("../assets/template/owner/topNav.php");
 	<br>
 	<br>
 	<div class="row">
-
+		<!--Contacts-->
 		<div class="col s12 m4 l2 hide-on-med-and-down">
 			<p class="black-text">Contacts</p>
 			<br>
 			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein - Company Name
-			<br><span class="error">M-F 9am - 5pm</span></p><br>
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
 			<p style="font-size:100%;"><span class="primary">Resident Sleeper</span> - <span class="primary">510-656-9196</span><br> Kelvin Lewin
-			<br><span class="error">M-F 9am - 5pm</span></p><br>
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
 			<p style="font-size:100%;"><span class="primary">Ganja Warrior</span> - <span class="primary">510-656-9196</span><br> Bob Marley
-			<br><span class="">M-Su 9am - 2am</span></p><br>
+				<br><span class="">M-Su 9am - 2am</span></p><br>
 			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein
-			<br><span class="error">M-F 9am - 5pm</span></p><br>
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
 			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein
-			<br><span class="error">M-F 9am - 5pm</span></p><br>
-
-
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
 		</div>
 
-
+		<!--activity-->
 		<div class="col s12 m12 l8">
 			<p>Displaying <span class="black-text">all activity <i class="fa fa-angle-down" aria-hidden="true"></i></span> by <span class="black-text">Date <i class="fa fa-angle-down" aria-hidden="true"></i></span></p>
 			<br>
@@ -63,7 +61,7 @@ include("../assets/template/owner/topNav.php");
 			</div>
 			<div class="row">
 				<div class="col s10">
-					<p>Nickolas Vaishampayan at Unit 21 has <span class="primary">my sink broke</span> on 6/30/2017</p>
+					<p>Nicholas Vaishampayan at Unit 21 has <span class="primary">my sink broke</span> on 6/30/2017</p>
 				</div>
 				<div class="col s2">
 					<p class="right">6/30/2017</p>
@@ -94,75 +92,83 @@ include("../assets/template/owner/topNav.php");
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m6 hide-on-large-only">
-			<p class="black-text">My Goals</p>
-			<br>
-			<p>There is stuff that goes here</p>
-		</div>
-		<style>
-			.row2 {
-				margin-bottom:0px;
-			}
-		</style>
+
+		<!--tasks-->
 		<div class="col s12 m6 l2">
 			<p class="black-text">My Tasks</p>
 			<br>
 			<!--this thing-->
+			<div class="row" style="margin:0; margin-bottom:20px;">
+				<div class="col s1" style="padding:0;">
+					<input type="checkbox" name="id1" value="1">
+				</div>
+				<div class="col s11" style="padding:0; padding-left:3px;">
+					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+			</div>
+			<div class="row" style="margin:0; margin-bottom:20px;">
+				<div class="col s1" style="padding:0;">
+					<input type="checkbox" name="id1" value="1">
+				</div>
+				<div class="col s11" style="padding:0; padding-left:3px;">
+					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+			</div>
+			<div class="row" style="margin:0; margin-bottom:20px;">
+				<div class="col s1" style="padding:0;">
+					<input type="checkbox" name="id1" value="1">
+				</div>
+				<div class="col s11" style="padding:0; padding-left:3px;">
+					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+			</div>
+			<div class="row" style="margin:0; margin-bottom:20px;">
+				<div class="col s1" style="padding:0;">
+					<input type="checkbox" name="id1" value="1">
+				</div>
+				<div class="col s11" style="padding:0; padding-left:3px;">
+					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+			</div>
+			<div class="row" style="margin:0; margin-bottom:20px;">
+				<div class="col s1" style="padding:0;">
+					<input type="checkbox" name="id1" value="1">
+				</div>
+				<div class="col s11" style="padding:0; padding-left:3px;">
+					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+			</div>
 
-			<div class="row" class="margin-bottom:0px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
-			<div class="row" class="margin-bottom:0px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
-			<div class="row" class="margin-bottom:0px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
-			<div class="row" class="margin-bottom:0px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
-			<div class="row" class="margin-bottom:0px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
 
 			<p class="black-text">My Upcomming Appointments</p>
 			<br>
-			<p>There is stuff that goes here</p><br>
-			<p>There is stuff that goes here</p><br>
-			<p>There is stuff that goes here</p><br>
-			<p>There is stuff that goes here</p><br>
-			<p>There is stuff that goes here</p><br>
-			<p>There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+			<p style="font-size:100%;">There is stuff that goes here</p><br>
+		</div>
+
+		<!--Contacts-->
+		<div class="col s12 m4 l2 hide-on-large-only">
+			<p class="black-text">Contacts</p>
+			<br>
+			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein - Company Name
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
+			<p style="font-size:100%;"><span class="primary">Resident Sleeper</span> - <span class="primary">510-656-9196</span><br> Kelvin Lewin
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
+			<p style="font-size:100%;"><span class="primary">Ganja Warrior</span> - <span class="primary">510-656-9196</span><br> Bob Marley
+				<br><span class="">M-Su 9am - 2am</span></p><br>
+			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
+			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein
+				<br><span class="error">M-F 9am - 5pm</span></p><br>
 		</div>
 	</div>
 </div>
