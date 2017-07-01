002da73ede5fac191036afc54fbd5b2f5d16ce9d
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-30 22:17:24 -0700

    Container spacing

--- owner/index.php
+++ owner/index.php
@@ -8,11 +8,11 @@ include("../assets/template/owner/topNav.php");
 <br>
 	<div class="row">
 
-		<div class="col s12 m4">
+		<div class="col s12 m4 l4">
 			<p class="black-text">My Goals</p>
 			<p>There is stuff that goes here</p>
 		</div>
-		<div class="col s12 m6">
+		<div class="col s12 m8 l6">
 			<p>Displaying <span class="black-text">all activity</span> by <span class="black-text">1 filter</span></p>
 			<p>There is stuff that goes here</p><br>
 			<p>There is stuff that goes here</p><br>
@@ -22,7 +22,7 @@ include("../assets/template/owner/topNav.php");
 			<p>There is stuff that goes here</p><br>
 			<p>There is stuff that goes here</p><br>
 		</div>
-		<div class="col s12 m2">
+		<div class="col s12 m12 l2">
 			<p class="black-text">My Tasks</p>
 			<p>There is stuff that goes here</p><br>
 			<p>There is stuff that goes here</p><br>
