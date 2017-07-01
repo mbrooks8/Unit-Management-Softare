f3b8e43f9dc03a20fb53b7716a1e971e94eaefde
Afropenguin <mbrooks8@ucmerced.edu>
2017-07-01 13:41:39 -0700

    Updated Patel Tennis with Hestia

--- assets/template/tenant/topNav.php
+++ assets/template/tenant/topNav.php
@@ -15,7 +15,7 @@
 		<div class="sidebar " id="side">
 			<div class="sideHeader" style="text-align:center;">
 				<div class="sideHeader" style="text-align:center;">
-					<h1 class="white-text"><a href="../index" class="logo white-text">Patel Tennis</a></h1>
+					<h1 class="white-text"><a href="../index" class="logo white-text">Hestia</a></h1>
 				</div>
 			</div>
 			<div class="sideBody">
