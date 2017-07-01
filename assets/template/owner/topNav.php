bd15288560e78b6cb38038ef3b36b277ed8cb3ff
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-30 22:14:58 -0700

    Navigation changes

--- assets/template/owner/topNav.php
+++ assets/template/owner/topNav.php
@@ -43,21 +43,26 @@
 				}
 			</style>
 			<nav class="">
-				<div style="width:90%;">
-					<a href="./index" class="logo"><img src="../assets/img/logo.svg" style="height:100%;"></a>
+				<div style="navContainer">
+					<a href="./index" class="logo hide-on-small-only hide-on-large-only"><img src="../assets/img/logo.svg" style="height:100%;"></a>
+					<a href="./index" class="logo hide-on-med-and-down"><img src="../assets/img/logoText.svg" style="height:100%;"></a>
 					<ul class="left hide-on-small-only">
 						<li><a href="./index" title="Dashboard"><i class="fa fa-tachometer" aria-hidden="true"></i></a></li>
 						<li><a href="./tasks" title="My Tasks"><i class="fa fa-tasks" aria-hidden="true"></i></a></li>
 						<li><a href="./people" title="Tenants"><i class="fa fa-users" aria-hidden="true"></i></a></li>
 						<li><a href="./email" title="Email"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+						<li><h4>|</h4></li>
+						<li><a href="./tasks" title="My Tasks"><i class="fa fa-tasks" aria-hidden="true"></i></a></li>
+						<li><a href="./people" title="Tenants"><i class="fa fa-users" aria-hidden="true"></i></a></li>
+						<li><a href="./email" title="Email"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
 					</ul>
-					<ul class="right hide-on-small-only">
+					<ul class="right hide-on-med-and-down">
 						<li><a href="./alerts" title="Alerts"><i class="fa fa-bell" aria-hidden="true"></i></a></li>
 						<li><a href="./call" title="Call"><i class="fa fa-phone" aria-hidden="true"></i></a></li>
 						<li><input placeholder="Search..." class="navSearch"></li>
 						<li><a href="./profile" title="My Profile"><i class="fa fa-user-o" aria-hidden="true"></i></a></li>
 					</ul>
-					<ul class="right hide-on-med-and-up">
+					<ul class="right hide-on-large-only">
 						<li><a class="sideToggle"><i class="fa fa-bars" aria-hidden="true"></i></a></li>
 
 					</ul>
