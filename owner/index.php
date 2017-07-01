c9d8dfc738b6aa60965007285391a937486da256
Afropenguin <mbrooks8@ucmerced.edu>
2017-06-30 21:41:51 -0700

    dashboard design

--- owner/index.php
+++ owner/index.php
@@ -1,29 +1,43 @@
 <?php
 include("../assets/template/owner/topNav.php");
 ?>
+<br>
+<div class="container large">
+	<h5>Dashboard</h5>
+	<div class="row">
 
-
-
-
-
-
-<div class="container">
-
-	<div class="card">
-		Hello This should be a card if it worked
+		<div class="col s12 m4">
+			<p class="black-text">My Goals</p>
+			<p>There is stuff that goes here</p>
+		</div>
+		<div class="col s12 m6">
+			<p>Displaying <span class="black-text">all activity</span> by <span class="black-text">1 filter</span></p>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+		</div>
+		<div class="col s12 m2">
+			<p class="black-text">My Tasks</p>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p class="black-text">My Upcomming Appointments</p>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+			<p>There is stuff that goes here</p><br>
+		</div>
 	</div>
-
-	<div class="container">
-		test
-	</div>
-	<p class="primary">test</p>
-
 </div>
-
-
-
-
-
 
 
 
