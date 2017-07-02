9ecbe2b23e8df38b52cedf9b71246e72aebeb092
Afropenguin <mbrooks8@ucmerced.edu>
2017-07-02 13:42:01 -0700

    Testing styles

--- owner/issues.php
+++ owner/issues.php
@@ -8,60 +8,137 @@ include("../assets/template/owner/topNav.php");
 	<br>
 	<div class="row">
 		<!--tasks-->
-		<div class="col s12 m8">
+		<div class="col s12 m12">
 			<p class="black-text">Tenant's Workorders</p>
 			<br>
+
+
+
+
+			<table class="bordered highlight">
+				<thead>
+					<tr>
+						<th>Tenant</th>
+						<th>Workorder</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<h5>
+								<span class="black-text">Esteban Perez</span>
+								<span style="font-size:80%;">#12</span>
+							</h5>
+							<p style="font-size:100%;">9/19/2017 &bull;
+								<span class="primary">
+									<i class="fa fa-phone" aria-hidden="true"></i> 510-570-5088
+								</span>
+							</p>
+						</td>
+						<td>
+							<h5 class="primary">Poop is on my walls</h5>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<h5 class="responsive-text">
+								<span class="black-text">Esteban Perez</span>
+								<span style="font-size:80%;">#12</span>
+							</h5>
+							<p style="font-size:100%;">9/19/2017 &bull;
+								<span class="primary">
+									<i class="fa fa-phone" aria-hidden="true"></i> 510-570-5088
+								</span>
+							</p>
+						</td>
+						<td>
+							<h5 class="primary responsive-text">This one is responsive Text dont know what to do about it i liek more text cause its cool</h5>
+						</td>
+					</tr>
+
+					<tr>
+						<td>
+							<h5>
+								<span class="black-text">Esteban Perez</span>
+								<span style="font-size:80%;">#12</span>
+							</h5>
+							<p style="font-size:100%;">9/19/2017 &bull;
+								<span class="primary">
+									<i class="fa fa-phone" aria-hidden="true"></i> 510-570-5088
+								</span>
+							</p>
+						</td>
+						<td>
+							<h5 class="primary">My Toilet is Cracked and I dont know what to do about it i liek more text cause its cool</h5>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+
+
+
+
+
 			<!--this thing-->
-			<div class="row" style="margin:0; margin-bottom:20px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0; padding-left:3px;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
+			<div class="row">
+				<div class="col s12 m4">
+					<h5>
+						<span class="black-text">Esteban Perez</span>
+						<span style="font-size:80%;">#12</span>
+					</h5>
 					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				</div>
+
+				<div class="col s12 m8">
+					<h5 class="primary">Toilet Crack</h5>
 				</div>
 			</div>
 			<hr>
-			<div class="row" style="margin:0; margin-bottom:20px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
+			<div class="row" style="margin:0;">
+				<div class="col s12 m4 l4" style="padding-left:0;">
+					<h5 class="responsive-text">
+						<span class="black-text">Esteban Perez</span>
+						<span style="font-size:80%;">#12</span>
+					</h5>
 				</div>
-				<div class="col s11" style="padding:0; padding-left:3px;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
-			</div>
-			<div class="row" style="margin:0; margin-bottom:20px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0; padding-left:3px;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				<div class="col s12 m8 l8" style="padding-left:0;">
+					<h5 class="primary responsive-text">This one is repsonsive text My Toilet is Cracked and I dont know what to do about it i liek more text cause its cool</h5>
 				</div>
 			</div>
-			<div class="row" style="margin:0; margin-bottom:20px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
+			<div class="col s12" style="padding-left:0;">
+				<p class="responsive-text" style="font-size:100%;">9/19/2017 &bull;
+					<span class="primary">
+						<i class="fa fa-phone" aria-hidden="true"></i> 510-570-5088
+					</span>
+				</p>
+			</div>
+			<br>
+			<hr>
+			<div class="row" style="margin:0;">
+				<div class="col s12 m4 l4" style="padding-left:0;">
+					<h5>
+						<span class="black-text">Esteban Perez</span>
+						<span style="font-size:80%;">#12</span>
+					</h5>
 				</div>
-				<div class="col s11" style="padding:0; padding-left:3px;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
+				<div class="col s12 m8 l8" style="padding-left:0;">
+					<h5 class="primary">My Toilet is Cracked and I dont know what to do about it i liek more text cause its cool</h5>
 				</div>
 			</div>
-			<div class="row" style="margin:0; margin-bottom:20px;">
-				<div class="col s1" style="padding:0;">
-					<input type="checkbox" name="id1" value="1">
-				</div>
-				<div class="col s11" style="padding:0; padding-left:3px;">
-					<p class="primary" style="font-size:100%;">Call the Plumber to unclog Esteban's Toilet at Unit 12</p>
-					<p style="font-size:100%;">9/19/2017 &bull; <wbr><span class="primary">510-570-5088</span></p>
-				</div>
+			<div class="col s12" style="padding-left:0;">
+				<p style="font-size:100%;">9/19/2017 &bull;
+					<span class="primary">
+						<i class="fa fa-phone" aria-hidden="true"></i> 510-570-5088
+					</span>
+				</p>
 			</div>
+			<br>
+			<hr>
 		</div>
 
 		<div class="col s12 m12 l2">
-			<p class="black-text">Contacts</p>
+			<p class="black-text">Vendors</p>
 			<br>
 			<p style="font-size:100%;"><span class="primary">Fertilizer Specilist</span> - <span class="primary">510-656-9196</span><br> Armando Hussein - Company Name
 				<br><span class="error">M-F 9am - 5pm</span></p><br>
