ce9a54e9b59ec5f290558f163914dd48ad595aa5
Afropenguin <mbrooks8@ucmerced.edu>
2017-07-03 10:21:37 -0700

    updated css to make things look better

--- owner/contacts.php
+++ owner/contacts.php
@@ -2,7 +2,7 @@
 include("../assets/template/owner/topNav.php");
 ?>
 <br>
-<div class="container large">
+<div class="container">
 	<h5 class="black-text">Contacts <span class="right">6/30/2016 &bull; 23:38</span></h5>
 	<br>
 
@@ -12,12 +12,18 @@ include("../assets/template/owner/topNav.php");
 			<div class="row">
 				<div class="col s12">
 					<ul class="tabs" style="padding:0;">
-						<li class="tab col s3"><a href="#allContacts">All Contacts</a></li>
-						<li class="tab col s3"><a class="active" href="#tenants">Tenants</a></li>
+						<li class="tab col s3"><a class="active" href="#allContacts">All Contacts</a></li>
+						<li class="tab col s3"><a href="#tenants">Tenants</a></li>
 						<li class="tab col s3"><a href="#vendors">Vendors</a></li>
 					</ul>
 				</div>
-				<div class="button raised errorButton" title="Deny"><i class="fa fa-times white-text" aria-hidden="true"></i></div>
+				<div class="row">
+					<div>
+						<p>test</p>
+					</div>
+									<div class="button raised successButton" title="Add A Contact"><i class="fa fa-plus white-text" aria-hidden="true"></i> Add</div>
+					<div class="button raised successButton" title="Import Contacts"><i class="fa fa-download white-text" aria-hidden="true"></i> Import</div>
+				</div>
 				<div id="allContacts" class="col s12">Test 1</div>
 				<div id="tenants" class="col s12">Test 2</div>
 				<div id="vendors" class="col s12">Test 3</div>
