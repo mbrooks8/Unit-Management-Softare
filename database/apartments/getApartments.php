280f6442a883b875a5f48a05d3993f31885e4e82
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-07-03 04:27:47 -0700

    forgot to savve

--- database/apartments/getApartments.php
+++ database/apartments/getApartments.php
@@ -7,17 +7,22 @@ $managerId = $_SESSION['id'];
 
 $stmt = $conn->prepare("SELECT * FROM `apartment` WHERE managerId = ?");
 $stmt->bind_param("i", $managerId);
+if($stmt->execute())
+{
+	$result = $stmt->get_result();
 
-$stmt->execute();
-$result = $stmt->get_result();
-
-$object = "[";
-while ($myrow = $result->fetch_assoc()) {
-	$object.="{\"name\":\"".$myrow['name']."\",\"units\":".$myrow['units'].",\"vacancies\":".$myrow['vacancies'].",\"publicId\":\"".$myrow['publicId']."\",\"apartmentId\":".$myrow['apartmentId']."},";
+	$object = "[";
+	while ($myrow = $result->fetch_assoc()) {
+		$object.="{\"name\":\"".$myrow['name']."\",\"units\":".$myrow['units'].",\"vacancies\":".$myrow['vacancies'].",\"publicId\":\"".$myrow['publicId']."\",\"apartmentId\":".$myrow['apartmentId']."},";
+	}
+	$object = substr($object, 0, -1);
+	$object.="]";
+	echo $object;
+}else
+{
+	echo 2;
 }
-$object = substr($object, 0, -1);
-$object.="]";
-echo $object;
+
 
 mysqli_close($conn);
 ?>
