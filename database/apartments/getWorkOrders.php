d53cf6749cda8cff0f2f84e77ccff96ae438ed71
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-07-06 14:47:21 -0700

    allow both managers and tenants to get a list of work orders

--- database/apartments/getWorkOrders.php
+++ database/apartments/getWorkOrders.php
@@ -2,23 +2,43 @@
 include('../utility/connect.php');
 session_start();
 include("../utility/serverCheckLogin.php");
-include("../utility/serverCheckManager.php");
-$managerId = $_SESSION['id'];
+$userId = $_SESSION['id'];
 $apartmentId = $_POST['apartmentId'];
 
-$stmt = $conn->prepare("SELECT * FROM `workOrders` WHERE apartmentId IN (SELECT apartmentId FROM `apartment` where managerId = ?) AND apartmentId = ?");
-$stmt->bind_param("ii", $managerId, $apartmentId);
 
-$stmt->execute();
-$result = $stmt->get_result();
 
-$object = "[";
-while ($myrow = $result->fetch_assoc()) {
-	$object.="{\"dateSubmitted\":\"".$myrow['dateSubmitted']."\",\"expectedCompletion\":\"".$myrow['expectedCompletion']."\",\"title\":\"".$myrow['title']."\",\"status\":".$myrow['status'].",\"urgency\":".$myrow['urgency'].",\"completed\":".$myrow['completed'].",\"userId\":".$myrow['userId'].",\"orderId\":".$myrow['orderId'].",\"apartmentId\":".$myrow['apartmentId']."},";
+if($_SESSION['type'] == 1)
+{
+
+	$stmt = $conn->prepare("SELECT * FROM `workOrders` WHERE apartmentId IN (SELECT apartmentId FROM `apartment` where managerId = ?) AND apartmentId = ?");
+	$stmt->bind_param("ii", $userId, $apartmentId);
+
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	$object = "[";
+	while ($myrow = $result->fetch_assoc()) {
+		$object.="{\"dateSubmitted\":\"".$myrow['dateSubmitted']."\",\"expectedCompletion\":\"".$myrow['expectedCompletion']."\",\"title\":\"".$myrow['title']."\",\"status\":".$myrow['status'].",\"urgency\":".$myrow['urgency'].",\"completed\":".$myrow['completed'].",\"userId\":".$myrow['userId'].",\"orderId\":".$myrow['orderId'].",\"apartmentId\":".$myrow['apartmentId']."},";
+	}
+	$object = substr($object, 0, -1);
+	$object.="]";
+	echo $object;
+}else
+{
+	$stmt = $conn->prepare("SELECT * FROM `workOrders` WHERE userId = ?");
+	$stmt->bind_param("i", $userId);
+
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	$object = "[";
+	while ($myrow = $result->fetch_assoc()) {
+		$object.="{\"dateSubmitted\":\"".$myrow['dateSubmitted']."\",\"expectedCompletion\":\"".$myrow['expectedCompletion']."\",\"title\":\"".$myrow['title']."\",\"description\":\"".$myrow['description']."\",\"status\":".$myrow['status'].",\"urgency\":".$myrow['urgency'].",\"completed\":".$myrow['completed'].",\"userId\":".$myrow['userId'].",\"orderId\":".$myrow['orderId'].",\"apartmentId\":".$myrow['apartmentId']."},";
+	}
+	$object = substr($object, 0, -1);
+	$object.="]";
+	echo $object;
 }
-$object = substr($object, 0, -1);
-$object.="]";
-echo $object;
 
 
 mysqli_close($conn);
